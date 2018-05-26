7dd1695022e044763e17e51a0e72652fe038c8c6
www <www@www.ww>
2018-05-26 15:03:18 +0300

    trying to make my first libreelec package

--- packages/shell/mc/package.mk
+++ packages/shell/mc/package.mk
@@ -16,19 +16,40 @@
 #  along with OpenELEC.  If not, see <http://www.gnu.org/licenses/>.
 ################################################################################
 
-PKG_NAME="zip"
-PKG_VERSION="30"
-PKG_SHA256="f0e8bb1f9b7eb0b01285495a2699df3a4b766784c1765a8f1aeedf63c0806369"
+PKG_NAME="mc"
+PKG_VERSION="4.8.7"
 PKG_ARCH="any"
-PKG_LICENSE="Info-ZIP"
-PKG_SITE="http://www.info-zip.org/pub/infozip/"
-PKG_URL="$SOURCEFORGE_SRC/infozip/Zip%203.x%20%28latest%29/3.0/${PKG_NAME}${PKG_VERSION}.tar.gz"
+PKG_LICENSE="GPL"
+PKG_SITE="http://www.midnight-commander.org/"
+PKG_URL="http://www.midnight-commander.org/downloads/$PKG_NAME-$PKG_VERSION.tar.bz2"
 PKG_SOURCE_DIR="${PKG_NAME}${PKG_VERSION}"
-PKG_DEPENDS_TARGET="toolchain bzip2"
-PKG_SECTION="compress"
-PKG_SHORTDESC="zip: PKUNZIP compatible compression utility"
-PKG_LONGDESC="zip is a compression and file packaging utility for Unix, VMS, MSDOS, OS/2, Windows 9x/NT/XP, Minix, Atari, Macintosh, MVS, z/OS, Amiga, Acorn RISC, and other OS. It is analogous to a combination of the Unix commands tar(1) and compress(1) (or tar(1) and gzip(1)) and is compatible with PKZIP (Phil Katz's ZIP for MSDOS systems) and other major zip utilities."
-PKG_TOOLCHAIN="manual"
+PKG_DEPENDS_TARGET="glib slang toolchain glib"
+PKG_SECTION="shell/filemanager"
+PKG_SHORTDESC="mc: free cross-platform filemanager"
+PKG_LONGDESC="Midnight Commander - free cross-platform filemanager and clone of Norton Commander"
+#PKG_TOOLCHAIN="manual"
+#PKG_TOOLCHAIN="autotools"
+
+PKG_CONFIGURE_OPTS_TARGET="--prefix=/usr \
+							--exec-prefix=/usr \
+							--without-gpm-mouse \
+							--disable-vfs-cpio \
+							--disable-vfs-fish \
+							--disable-vfs-sfs \
+							--enable-vsf-extfs \
+							--without-mmap \
+								--with-subshell \
+							--with-edit \
+							--without-x \
+							--enable-charset \
+							--enable-background \
+							--with-screen=slang \
+							fu_cv_sys_stat_statfs2_bsize=yes"
+							
+pre_configure_target() { 
+	export LDFLAGS="$LDFLAGS -lvchiq_arm"
+}
+
 
 make_target() {
   make CC=$CC CPP=$CPP RANLIB=$RANLIB AR=$AR STRIP=$STRIP LOCAL_ZIP="$CFLAGS" \
