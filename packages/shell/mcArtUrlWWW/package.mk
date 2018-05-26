cfb65840bcee0ec1a665108ab8db8c3fccf02e62
www <www@www.ww>
2018-05-26 19:02:48 +0300

    Trying to add mc to build

--- packages/shell/mcArtUrlWWW/package.mk
+++ packages/shell/mcArtUrlWWW/package.mk
@@ -1,35 +1,32 @@
 ################################################################################
-#      This file is part of OpenELEC - http://www.openelec.tv
-#      Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
+#      This file is part of LibreELEC - https://libreelec.tv
+#      Copyright (C) 2016-present Team LibreELEC
+#      Copyright (C) 2009-2012 Stephan Raue (stephan@openelec.tv)
 #
-#  OpenELEC is free software: you can redistribute it and/or modify
+#  LibreELEC is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
 #  the Free Software Foundation, either version 2 of the License, or
 #  (at your option) any later version.
 #
-#  OpenELEC is distributed in the hope that it will be useful,
+#  LibreELEC is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #  GNU General Public License for more details.
 #
 #  You should have received a copy of the GNU General Public License
-#  along with OpenELEC.  If not, see <http://www.gnu.org/licenses/>.
+#  along with LibreELEC.  If not, see <http://www.gnu.org/licenses/>.
 ################################################################################
 
-PKG_NAME="mcArtUrlWWW"
-PKG_VERSION="4.8.7"
+PKG_NAME="mc"
+PKG_VERSION="4.8.20"
+PKG_SHA256="017ee7f4f8ae420a04f4d6fcebaabe5b494661075c75442c76e9c8b1923d501c"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
-PKG_SITE="http://www.midnight-commander.org/"
-#PKG_URL="http://www.midnight-commander.org/downloads/$PKG_NAME-$PKG_VERSION.tar.bz2"
-PKG_URL="http://www.midnight-commander.org/downloads/mc-$PKG_VERSION.tar.bz2"
-#PKG_SOURCE_DIR="${PKG_NAME}${PKG_VERSION}"
+PKG_SITE="http://www.midnight-commander.org"
+PKG_URL="http://ftp.midnight-commander.org/mc-${PKG_VERSION}.tar.xz"
 PKG_DEPENDS_TARGET="toolchain gettext:host glib libssh2 libtool:host ncurses pcre"
-PKG_SECTION="shell/filemanager"
-PKG_SHORTDESC="mc: free cross-platform filemanager"
-PKG_LONGDESC="Midnight Commander - free cross-platform filemanager and clone of Norton Commander"
-#PKG_TOOLCHAIN="manual"
-#PKG_TOOLCHAIN="autotools"
+PKG_SECTION="tools"
+PKG_LONGDESC="Midnight Commander is a text based filemanager that emulates Norton Commander"
 
 PKG_CONFIGURE_OPTS_TARGET=" \
   --datadir=/storage/.kodi/addons/virtual.system-tools/data \
@@ -53,13 +50,12 @@ PKG_CONFIGURE_OPTS_TARGET=" \
   --enable-vfs-sftp \
   --enable-vfs-tar \
   --without-x"
-							
-pre_configure_target() { 
-	LDFLAGS="$LDFLAGS -lcrypto -lssl"
+
+pre_configure_target() {
+  LDFLAGS="$LDFLAGS -lcrypto -lssl"
 }
 
-makeinstall_target() {
-  mkdir -p $INSTALL/usr/bin
-    cp zip $INSTALL/usr/bin
-    $STRIP $INSTALL/usr/bin/zip
+post_makeinstall_target() {
+  rm -rf $INSTALL/storage/.kodi/addons/virtual.system-tools/data/locale
+  rm -rf $INSTALL/storage/.kodi/addons/virtual.system-tools/data/mc/help/mc.hlp.*
 }
